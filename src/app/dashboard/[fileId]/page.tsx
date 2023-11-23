5ce816baa5e98c26669419c0cabd91db1585ad3c
inam95 <mfihassan99@gmail.com>
2023-11-23 07:58:26 +0530

    pdf renderer

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -32,7 +32,7 @@ export default async function FileViewPage({ params }: FileViewPageProps) {
         {/* Left Side */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
